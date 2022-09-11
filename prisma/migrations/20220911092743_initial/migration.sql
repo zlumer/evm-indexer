a95aa55bdad0fc5a85d0372b26900608139525f7
zlumer <zlumer@gmail.com>
2022-09-11 18:01:48 +0800

    switch to postgres

--- prisma/migrations/20220911092743_initial/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `index_blocks_processed` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `timestamp` int unsigned NOT NULL,
-    `last_block_processed` int unsigned NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
