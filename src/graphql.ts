b68d690a4c73306debd2d99d7badf939844dd14c
zlumer <zlumer@gmail.com>
2022-09-25 18:42:26 +0800

    graphql route usage

--- src/graphql.ts
+++ src/graphql.ts
@@ -2,20 +2,25 @@ import express from "express"
 import postgraphile from "postgraphile"
 import type { ClientConfig } from "pg"
 
-export async function startGraphqlServer(dbUrl: string | ClientConfig, port: string | number)
+export function getGraphqlRoute(dbUrl: string | ClientConfig)
 {
-	let app = express()
-
-	app.use(express.json())
-
-	app.use(postgraphile(dbUrl, "public", {
+	return postgraphile(dbUrl, "public", {
 		// watchPg: true, // not really needed -- we will restart the server on schema changes
 		simpleCollections: "both",
 		dynamicJson: true,
 		enableCors: true,
 		disableDefaultMutations: true,
 		disableQueryLog: true,
-	}))
+	})
+}
+
+export async function startGraphqlServer(dbUrl: string | ClientConfig, port: string | number)
+{
+	let app = express()
+
+	app.use(express.json())
+
+	app.use(getGraphqlRoute(dbUrl))
 
 	console.log(`starting GraphQL server on port ${port}...`)
 	return new Promise<express.Express>(res =>
